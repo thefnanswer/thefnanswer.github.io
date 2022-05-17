12f6003465116d22aaeab90e4c4818a0af2eb661
thefnanswer <timothy.w.sweet@gmail.com>
2022-05-16 20:07:45 -0400

    update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
 var $html = document.getElementsByTagName( 'html' )[0];
-
-var $menu_opener = document.getElementById( 'nav-jump' ),
+    $menu_opener = document.getElementById( 'nav-jump' ),
     $menu_closer = document.getElementById( 'menu-close' );
