ee63b1f3e77bf39243882923d4951f97b5f37269
thefnanswer <timothy.w.sweet@gmail.com>
2022-06-03 17:12:22 -0400

    updated HTML

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 var $html = document.getElementsByTagName( 'html' )[0];
     $menu_opener = document.getElementById( 'nav-jump' ),
     $menu_closer = document.getElementById( 'menu-close' );
+
+var class_names;
+
+class_names = $html.className;
+class_names = $html.getAttribute( 'class' );
