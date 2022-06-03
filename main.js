68c3c212918d04bd4d5b2d34c74ef5491a9740f7
thefnanswer <timothy.w.sweet@gmail.com>
2022-06-03 17:35:53 -0400

    updated HTML

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var $html = document.getElementsByTagName( 'html' )[0];
-    $menu_opener = document.getElementById( 'nav-jump' ),
+var $menu_opener = document.getElementById( 'nav-jump' ),
     $menu_closer = document.getElementById( 'menu-close' );
 
-var class_names;
+$menu_opener
+    .addEventListener( 'click', toggleDrawerNav, false );
+$menu_closer
+    .addEventListener( 'click', toggleDrawerNav, false );
 
-class_names = $html.className;
-class_names = $html.getAttribute( 'class' );
