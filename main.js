9f1795c920f399e27c2df7a59336677c5f098238
thefnanswer <timothy.w.sweet@gmail.com>
2022-06-03 17:45:59 -0400

    update JSS

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 var $menu_opener = document.getElementById( 'nav-jump' ),
     $menu_closer = document.getElementById( 'menu-close' );
-
-$menu_opener
-    .addEventListener( 'click', toggleDrawerNav, false );
-$menu_closer
-    .addEventListener( 'click', toggleDrawerNav, false );
-
+    $menu_opener
+        .addEventListener( 'click', toggleDrawerNav, false );
+    $menu_closer
+        .addEventListener( 'click', toggleDrawerNav, false );
+    
+var setTimeout(
+        Function(){
+            toggleDrawerNav_running = false;
+        },
+        500
+    );
