2c6789facb632247cdbc66561fd65315eca7ce27
thefnanswer <timothy.w.sweet@gmail.com>
2022-06-08 15:31:22 -0400

    update main js

--- main.js
+++ main.js
@@ -30,14 +30,33 @@ class_names = class_names
 class_names = class_names
                 .replace(/\bmy-new-class-name\b/, '');
 
-class_names = class_names.split('');
-
-var len = class_names.length,
-    kept_classes = [];
-while (len--) {
-    if (class_names[len] != 'my-new-class-name') {
-        kept_classes.push(class_names[len]);
-    }
+function removeNewClassFromHTML() {
+  var class_names = class_names.split('');
+      len = class_names.length,
+      kept_classes = [];
+  while (len--) {
+      if (class_names[len] != 'my-new-class-name') {
+          kept_classes.push(class_names[len]);
+      }
+  }
+  class_names = kept_classes.join('');
 }
 
-class_names = kept_classes.join('');
+var removeNewClassFromHTML = function(){};
+var addNewClassToHTML = function(){};
+
+window.removeNewClassFromHTML; // function(){}
+window.addNewClassToHTML;      // function(){}
+
+function addClass(element, class_name) {
+  var class_names = element.className.split('');
+  class_names.push(class_name);
+  element.className = class_names.join('');
+}
+
+$html.classList; // a collection of class names
+
+$html.classList.add('my-new-class-name');
+$html.classList.remove('my-new-class-name');
+$html.classList.toggle('my-new-class-name');
+$html.classList.contains('my-new-class-name');
